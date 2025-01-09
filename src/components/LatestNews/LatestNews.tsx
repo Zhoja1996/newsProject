9014f123e5759651c0f5e52ba4d518a5936aaff6
Zhoja1996 <g.n.kowal@gmail.com>
2025-01-09 18:11:57 +0200

    migrate from js to ts

---
+++ src/components/LatestNews/LatestNews.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { getLatestNews } from "../../api/apiNews";
+import { useFetch } from "../../helpers/hooks/useFetch";
+import { NewsApiResponse } from "../../interfaces";
+import BannersList from "../BannersList/BannersList";
+import styles from "./styles.module.css";
+
+const LatestNews = () => {
+  const { data, isLoading } = useFetch<NewsApiResponse, null>(getLatestNews);
+
+  return (
+    <section className={styles.section}>
+      <BannersList banners={data && data.news} isLoading={isLoading} />
+    </section>
+  );
+};
+
+export default LatestNews;
