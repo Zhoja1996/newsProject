56a3cc1d517725b7b1cde0a3e6883a59609e2ac3
Zhoja1996 <g.n.kowal@gmail.com>
2025-01-23 13:56:20 +0200

    add react-router

--- src/app/appEntry.tsx
+++ src/app/appEntry.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { store } from './appStore'
-import BaseLayout from './layouts/BaseLayout'
 import { ThemeProvider } from './providers/ThemeProvider'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import '@/shared/index.css';
+import { RouterProvider } from 'react-router-dom'
+import { appRouter } from './appRouter'
 
 createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ThemeProvider>
             <Provider store={store}>
-                <BaseLayout />
+                <RouterProvider router={appRouter} />
             </Provider>
         </ThemeProvider>
     </React.StrictMode>,
--- src/widgets/header/ui/Header/Header.tsx
+++ src/widgets/header/ui/Header/Header.tsx
@@ -1,5 +1,6 @@
 import { useTheme } from "@/app/providers/ThemeProvider";
 import { formatDate } from "@/shared/helpers/formatDate";
+import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 import ThemeButton from "@/features/theme/ui/ThemeButton/ThemeButton";
 
@@ -10,7 +11,10 @@ const Header = () => {
     return (
         <header className={`${styles.header} ${isDarkMode ? styles.dark : styles.light}`}>
             <div className={styles.info}>
-                <h1 className={styles.title}>NEWS REACTIFY</h1>
+                <Link to="/">
+                    <h1 className={styles.title}>NEWS REACTIFY</h1>
+                </Link>
+
                 <p className={styles.date}>{formatDate(new Date())}</p>
             </div>
 
